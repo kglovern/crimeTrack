45c04933ca7aaa833926309caa774376e382e9e9
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2017-03-29 09:59:31 -0400

    Updated shell script

--- crimeTrack.sh
+++ crimeTrack.sh
@@ -5,14 +5,20 @@
 # Team Sarnia - Quinton Di Paolo, Jackson Firth, Kevin Glover-Netherton, Mitch Haflidson
 #
 
+# Make PDF Directory to store output if it doesn't exist
+[ ! -d "PDF" ] && mkdir PDF
+# Make Question directory to store past queries
+[ ! -d "QUERY"] && mkdir QUERY
+
 #Make an output pdf file name
 NOW=$(date +'%H%M%S')
-OUTFILE="output-$NOW.pdf"
+OUTFILE="PDF/output-$NOW.pdf"
+INFILE="QUERY/input-$NOW.pdf"
 
 # Run the user interface to generate the question query
-perl uInterface.pl
+perl uInterface.pl $INFILE
 # Generate data requested by the query
-perl query.pl input.que
+perl query.pl $INFILE
 # Generate a pdf output
 clear
 perl plotCrime.pl output.data $OUTFILE
