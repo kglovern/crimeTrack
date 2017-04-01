c297701c2dfa1915c7cc5774e37bffab9c0c47bb
socs <socs@socsvm>
2017-04-01 12:13:47 -0400

    Program takes cities from csv file and places them under the province they're in (with index numbers)

--- genCities.pl
+++ genCities.pl
@@ -8,13 +8,19 @@ use warnings;
 use version;   our $VERSION = qv('5.16.0');
 use Text::CSV  1.32;
 
-
 my $fh;
 my $csv = Text::CSV->new({ binary => 1});
 my @records;
 my @cities;
+my @newGroup;
 my %provinces;
+my %groups;
 my $cityCount = 0;
+
+sub removeRep {
+  my %rep;
+  grep !$rep{$_}++, @_;
+}
 
 open $fh, "<", "./data/2015Crime.csv"
    or die "Unable to open file";
@@ -26,20 +32,26 @@ while (my $record = <$fh>) {
    my @fields = $csv->fields;
    my $loc = $fields[0];
    if ($loc =~ /^[a-z\s]*$/i) {
-      print "$loc is a province\n";
+      #print "$loc is a province\n";
       if (not exists $provinces{$loc} and $loc ne "Canada") {
          $provinces{$loc} = {};
       }
    } else {
-      my @parts = split (", ", $loc);
-      my $city = $parts[0];
-      my $province = $parts[1];
-      print "$city is the city, $province is the province\n";
+      my ($city, $province) = split (", ", $loc);
+      push(@{$groups{$province}}, $city);
    }
 }
-
+foreach my $group (keys %groups) {
+   print "$group\n";
+   @newGroup = removeRep(@{$groups{$group}});
+   foreach my $element (@newGroup) {
+     my ($index) = grep {$newGroup[$_] ~~ $element} 0..$#newGroup;
+     print "\t$element, $index\n";
+   }
+}
 while (my ($province, $cities) = each %provinces) {
-   print "$province\n";
+   #print "$province\n";
 }
 
 close $fh;
+
