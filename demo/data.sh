c0210e007591fabd0b6541dcb099c79c5d95bbdb
Kevin Glover-Netherton <kevin.glovernetherton@gmail.com>
2017-04-05 15:14:09 -0400

    Made an data format example

---
+++ demo/data.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+clear
+echo "The following is an example of the aggregated crime files"
+read -rsp "Press enter to continue"
+echo ""
+head data/1998Crime.csv
+echo ""
+read -rsp "Press enter to continue"
+echo ""
+echo "The following is an example of the locations aggregation"
+read -rsp "Press enter to continue"
+echo ""
+head data/locs.data
+read -rsp "Press enter to continue"
+echo ""
+echo "The following is an example of a simple query"
+read -rsp "Press enter to continue"
+echo ""
+echo ""
+head demo/d1.que
+echo ""
